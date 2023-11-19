ef7afc3cfaa7d2e4798ada382a3891cbb1471614
GaziWani1 <gaziwani123@gmail.com>
2023-11-19 14:17:24 +0530

    userroutes.

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect('mongodb://localhost:27017');
+        const conn = await mongoose.connect('mongodb://localhost:27017/goalTracker');
         console.log(`mongoDB connected : ${conn.connection.host}`)
     } catch (error) {
         console.log(error)
